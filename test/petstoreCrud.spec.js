48bb002bc06539552a648774463417d260eb03be
MariaMarkel <volkova0512@gmail.com>
2022-06-02 22:31:18 -0400

    update package.json petstoreCrud.spec.js

--- test/petstoreCrud.spec.js
+++ test/petstoreCrud.spec.js
@@ -1,35 +1,35 @@
 const axios = require ('axios');
 
-it ('Create: add a new pet to store', function () {
-    const data = JSON.stringify({
-        "id": 1160,
-        "category": {
+const data = JSON.stringify({
+    "id": Math.floor(Math.random()*1001 + 1000),
+    "category": {
+        "id": 0,
+        "name": "string"
+    },
+    "name": "Taco",
+    "photoUrls": [
+        "string"
+    ],
+    "tags": [
+        {
             "id": 0,
             "name": "string"
-        },
-        "name": "Susanna",
-        "photoUrls": [
-            "string"
-        ],
-        "tags": [
-            {
-                "id": 0,
-                "name": "string"
-            }
-        ],
-        "status": "available"
-    });
+        }
+    ],
+    "status": "available"
+});
 
-    const config = {
-        method: 'post',
-        url: 'https://petstore.swagger.io/v2/pet',
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        data: data
-    };
+const config = {
+    method: 'post',
+    url: 'https://petstore.swagger.io/v2/pet',
+    headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    data: data
+};
 
+it ('Create: add a new pet to store', function () {
     axios(config)
         .then(function (response) {
             console.log(JSON.stringify(response.data));
