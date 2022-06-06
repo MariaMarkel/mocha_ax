33a800696f667d4347697b2cc81eb50f044fc728
MariaMarkel <volkova0512@gmail.com>
2022-06-05 21:07:49 -0400

    add GET request

--- test/petstoreCrud.spec.js
+++ test/petstoreCrud.spec.js
@@ -28,13 +28,29 @@ const config = {
     },
     data: data
 };
+let petId;
 
-it ('Create: add a new pet to store', function () {
-    axios(config)
+it ('Create: add a new pet to store', async function () {
+    await axios(config)
         .then(function (response) {
             console.log(JSON.stringify(response.data));
+            petId = response.data.id;
         })
         .catch(function (error) {
             console.log(error);
         });
 });
+it ('Read: find pet by id', function () {
+    const config = {
+        method: 'get',
+        url: `https://petstore.swagger.io/v2/pet/${petId}`,
+        headers: { }
+    };
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error.response.statusText);
+        });
+})
